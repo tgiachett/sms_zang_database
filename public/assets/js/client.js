ea8a0ccdb295abf1f97d3f113f2822f67001f2bc
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-08-28 15:33:00 -0700

    removed ws code temp

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,4 +1,16 @@
 
+ 
+// const ws = new WebSocket('ws://8080');
+ 
+// ws.on('open', function open() {
+//   ws.send('something');
+// });
+ 
+// ws.on('message', function incoming(data) {
+//   console.log(data);
+// });
+
+
 $(document).ready(function() {
 
   //TRIGGER DIARY TEXT MODAL
