9e70249139706df9f6d1f5ce9ba0ac6b608a2765
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-08-30 08:45:09 -0700

    following dataValues as indicated in sequelize

--- bin/server.js
+++ bin/server.js
@@ -35,8 +35,8 @@ models.sequelize.sync().then(function() {
     
     let broad = setInterval(function () {
       models.Entry.findAll({}).then((dbEntries) => {
-        console.log(JSON.parse(dbEntries))
-        wss.broadcast(JSON.parse(dbEntries))
+        console.log(dbEntries.dataValues)
+        wss.broadcast(dbEntries.dataValues)
 
       });
         
