5f5c90f93bae8642edfc8f6c270d814d3b55dc8b
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-08-31 10:17:18 -0700

    broadcast needed to be defined

--- bin/server.js
+++ bin/server.js
@@ -33,6 +33,14 @@ models.sequelize.sync().then(function() {
    
    wss = wSocket.server(server)
 
+   wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  };
+  
    wss.on('connection', function connection(ws) {
     ws.send("Connected")
 
